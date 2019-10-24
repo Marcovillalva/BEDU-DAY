ae7900a7612dccaa1269747d30c67420e0379900
Marcovillalva <spiller31@hotmail.com>
2019-10-24 17:36:27 -0500

    agrego video en conUno y contDos para valores agregados

--- src/components/Layout/Section.js
+++ src/components/Layout/Section.js
@@ -6,7 +6,16 @@ class Section extends Component {
     render() {
         return (
             <div className="sectionMainCont">
-                section
+                <div className="contUno">
+                <h1>UXMAL STREAM</h1>
+               
+                <p>Uxmal crea experiencias. No solo es ambientar especios, se trata de conectar
+                    a la gente mediante emociones, recuerdos y sentimientos positivos</p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/s6XoZ7TnEEg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className="contDos">
+
+                </div>
             </div>
         );
     }
